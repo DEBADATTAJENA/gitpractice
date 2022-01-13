b8e1c2af8c7611f7e8eaf1137acaeda1ea1d1da0
DEBADATTAJENA <75190799+DEBADATTAJENA@users.noreply.github.com>
2022-01-13 19:29:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Welcome to the Git world");
-        System.out.println("One world");init
+        System.out.println("One world");
     }
 }
